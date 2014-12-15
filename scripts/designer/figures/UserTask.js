2cb8df123303b20d10c007e722ad9971e415750b
ranji1221 <jiran1221@163.com>
2014-12-15 17:53:15 +0800

    INIT

--- scripts/designer/figures/UserTask.js
+++ scripts/designer/figures/UserTask.js
@@ -1,14 +1,20 @@
-/*
- * 项目说明：Activiti-Designer是为业务人员、开发人员及系统管理员用图形化方式方便友好的设计工作流而开发的项目。
- * 版权所有 (C) 2014 纪冉
- * 这一程序是自由软件，你可以遵照自由软件基金会出版的GNU通用公共许可证条款来修改和重新发布这一程序。或者用许可证的第二版，或者（根据你的选择）用任何更新的版本。
- * 发布这一程序的目的是希望它有用，但没有任何担保。甚至没有适合特定目的的隐含的担保。更详细的情况请参阅GNU通用公共许可证。
- * 你应该已经和程序一起收到一份GNU通用公共许可证的副本。如果还没有，
- * 写信给：
- * 	 The Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
- *   MA02139, USA
- * 联系方式：QQ(95724368) 泥鳅也是鱼 E-Mail (jiran1221@163.com)
-*/
+/**
+ * Activiti5 Designer 0.0.1
+ * 
+ * Copyright (c) 2014 RanJi All rights reserved.
+ * 
+ * This program is free software, and you can follow the Free Software Foundation's GNU General Public License as published by the terms used to modify and redistribute the program. 
+ * Or a second license, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY. 
+ * Without even the implied warranty for a particular purpose. More detailed information, please refer to the GNU General Public License.
+ * You should have received a copy of the GNU General Public License along with the program. If not,
+ * Visit the WebSite:
+ *	 Licensed under the GPL license: http://www.gnu.org/licenses/gpl.txt
+ * 
+ * To use it on other projects please contact me at jiran1221@163.com
+ * Thanks for your using. Good Luck!
+ * 
+ */
 
 
 org.ranji.activiti.UserTaskICON = draw2d.shape.icon.User.extend({
